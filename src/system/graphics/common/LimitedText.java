def6e4ffea0342868805513ef6b37ed0d11479a8
Kristjan Hendrik Küngas <kristjankyngas@outlook.com>
2016-05-10 22:37:54 +0300

    limitedtext test

---
+++ src/system/graphics/common/LimitedText.java
@@ -0,0 +1,40 @@
+package system.graphics.common;
+
+import javafx.scene.text.Text;
+
+public class LimitedText extends Text {
+    private int lengthLimit;
+    private String originalText;
+
+    public LimitedText() {
+    }
+
+    public LimitedText(String text) {
+        super(text);
+    }
+
+    public String getOriginalText() {
+        return this.originalText;
+    }
+
+    public void setOriginalText(String limitedtext) {
+        this.originalText = limitedtext;
+        if (limitedtext.length() > this.lengthLimit) {
+            super.setText(limitedtext.substring(0, lengthLimit) + "...");
+            System.out.println("limited text set");
+        } else {
+            super.setText(limitedtext);
+            System.out.println("normal text set");
+        }
+
+    }
+
+    public String getLengthLimit() {
+        return String.valueOf(this.lengthLimit);
+    }
+
+    public void setLengthLimit(String limit) {
+        this.lengthLimit = Integer.parseInt(limit);
+        System.out.println("limit set");
+    }
+}
