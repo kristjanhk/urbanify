4200ba0e6d31a929b347dbe6350307dcc170d933
Kristen Kotkas <kristen.kotkas@gmail.com>
2016-05-11 00:33:12 +0300

    report fxml +  ticket

--- src/system/graphics/report/Ticket.java
+++ src/system/graphics/report/Ticket.java
@@ -2,6 +2,8 @@ package system.graphics.report;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.StrokeType;
@@ -13,18 +15,18 @@ import java.util.ArrayList;
 
 /**
  * Pilet aruande stseenis
- * On HBoxi mähisklass
+ * On GridPane mähisklass
  * Luuakse programmi töö käigus, seega ei saa FXMLis ette valmistada
  */
-public class Ticket extends HBox{
+public class Ticket extends GridPane{
 
     public Ticket(String ticketname, ArrayList<Double> ticketdata) {
         this.setAlignment(Pos.CENTER_RIGHT);
-        this.setMaxSize(715.0, 50.0);
-        this.setMinSize(715.0, 50.0);
-        this.setPrefSize(715.0, 50.0);
+        this.setMaxSize(750, 50.0);
+        this.setMinSize(750, 50.0);
+        this.setPrefSize(750, 50.0);
         this.getStyleClass().add("textFlow30");
-        VBox.setMargin(this, new Insets(0.0, 0.0, 20.0, 0.0));
+        GridPane.setMargin(this, new Insets(0.0, 0.0, 20.0, 0.0));
         this.initChildren(ticketname, ticketdata);
     }
 
@@ -33,36 +35,40 @@ public class Ticket extends HBox{
         ticket.getStyleClass().add("text35");
         ticket.setStrokeType(StrokeType.OUTSIDE);
         ticket.setStrokeWidth(0.0);
-        ticket.setWrappingWidth(180.0);
-        HBox.setMargin(ticket, new Insets(0.0, 0.0, 0.0, 20.0));
-        this.getChildren().add(ticket);
+        ticket.setWrappingWidth(242.0);
+        GridPane.setMargin(ticket, new Insets(0.0, 0.0, 0.0, 20.0));
+        this.add(ticket,0,0);
+        this.getColumnConstraints().add(new ColumnConstraints(242,242,242));
 
         Text cost = new Text(String.format("%.2f", ticketdata.get(0)) + " " + Lang.getActiveLang().getCurrency());
         cost.getStyleClass().add("text35");
         cost.setStrokeType(StrokeType.OUTSIDE);
         cost.setStrokeWidth(0.0);
-        cost.setWrappingWidth(134.0);
+        cost.setWrappingWidth(125.0);
         cost.setTextAlignment(TextAlignment.CENTER);
-        HBox.setMargin(cost, new Insets(0.0, 0.0, 0.0, 3.0));
-        this.getChildren().add(cost);
+        GridPane.setMargin(cost, new Insets(0.0, 0.0, 0.0, 0));
+        this.add(cost,1,0);
+        this.getColumnConstraints().add(new ColumnConstraints(125,125,125));
 
         Text quantity = new Text(String.valueOf(ticketdata.get(1).intValue()));
         quantity.getStyleClass().add("text35");
         quantity.setStrokeType(StrokeType.OUTSIDE);
         quantity.setStrokeWidth(0.0);
-        quantity.setWrappingWidth(188.0);
+        quantity.setWrappingWidth(200.0);
         quantity.setTextAlignment(TextAlignment.CENTER);
-        HBox.setMargin(quantity, new Insets(0.0, 0.0, 0.0, 44.0));
-        this.getChildren().add(quantity);
+        GridPane.setMargin(quantity, new Insets(0.0, 0.0, 0.0, 0));
+        this.add(quantity,2,0);
+        this.getColumnConstraints().add(new ColumnConstraints(200,200,200));
 
         Text total = new Text(String.format("%.2f", ticketdata.get(0) * ticketdata.get(1)) + " " +
                 Lang.getActiveLang().getCurrency());
         total.getStyleClass().add("text35");
         total.setStrokeType(StrokeType.OUTSIDE);
         total.setStrokeWidth(0.0);
-        total.setWrappingWidth(136.0);
+        total.setWrappingWidth(175.0);
         total.setTextAlignment(TextAlignment.CENTER);
-        HBox.setMargin(total, new Insets(0.0, 0.0, 0.0, 12.0));
-        this.getChildren().add(total);
+        GridPane.setMargin(total, new Insets(0.0, 0.0, 0.0, 0));
+        this.add(total,3,0);
+        this.getColumnConstraints().add(new ColumnConstraints(175,175,175));
     }
 }
