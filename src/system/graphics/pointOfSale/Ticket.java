d1cb0da23508e99dc5db6da582bca65fb753b7b9
Kristen Kotkas <kristen.kotkas@gmail.com>
2016-05-12 00:43:44 +0300

    pos valmis vist

--- src/system/graphics/pointOfSale/Ticket.java
+++ src/system/graphics/pointOfSale/Ticket.java
@@ -4,6 +4,8 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.StrokeType;
@@ -12,7 +14,7 @@ import javafx.scene.text.TextAlignment;
 import system.data.Event;
 import system.data.Lang;
 
-public class Ticket extends HBox {
+public class Ticket extends GridPane {
     private Controller parentController;
     private Event event;
     private Text ticketName;
@@ -29,7 +31,7 @@ public class Ticket extends HBox {
         this.tickettype = ticketName;
         this.event = event;
         this.setAlignment(Pos.CENTER_LEFT);
-        VBox.setMargin(this, new Insets(0, 40.0, 25.0, 0));
+        VBox.setMargin(this, new Insets(0, 0, 20, 0));
         this.initChildren();
     }
 
@@ -54,30 +56,34 @@ public class Ticket extends HBox {
         this.ticketName.getStyleClass().add("text35");
         this.ticketName.setStrokeType(StrokeType.OUTSIDE);
         this.ticketName.setStrokeWidth(0.0);
-        this.ticketName.setWrappingWidth(150.0);
-        this.getChildren().add(this.ticketName);
+        this.ticketName.setWrappingWidth(280);
+        GridPane.setMargin(this.ticketName, new Insets(0.0, 0.0, 0.0, 0.0));
+        this.add(this.ticketName,0,0);
+        this.getColumnConstraints().add(new ColumnConstraints(280,280,280));
 
         this.removeTicket = new Button("-");
         this.removeTicket.getStyleClass().add("buttonRound");
         this.removeTicket.setMnemonicParsing(false);
-        HBox.setMargin(this.removeTicket, new Insets(0.0, 0.0, 0.0, 20.0));
+        HBox.setMargin(this.removeTicket, new Insets(0.0, 0.0, 0.0, 0.0));
         this.removeTicket.setOnMouseClicked(event -> {
             if (this.ticketAmount.getValue() > 0) {
                 this.ticketAmount.set(this.ticketAmount.getValue() - 1);
                 this.parentController.freeSeat(this.event.getTicketPrice(this.tickettype));
             }
         });
-        this.getChildren().add(this.removeTicket);
+        this.add(this.removeTicket,1,0);
+        this.getColumnConstraints().add(new ColumnConstraints(70,70,70));
 
         Text ticketAmountText = new Text("0");
         ticketAmountText.getStyleClass().add("text35");
         ticketAmountText.setTextAlignment(TextAlignment.CENTER);
         ticketAmountText.setStrokeType(StrokeType.OUTSIDE);
         ticketAmountText.setStrokeWidth(0.0);
-        ticketAmountText.setWrappingWidth(58.0); //57.8671875??
-        HBox.setMargin(ticketAmountText, new Insets(0.0, 20.0, 0.0, 20.0));
+        ticketAmountText.setWrappingWidth(100.0); //57.8671875??
+        HBox.setMargin(ticketAmountText, new Insets(0.0, 0, 0.0, 0));
         ticketAmountText.textProperty().bind(this.ticketAmount.asString());
-        this.getChildren().add(ticketAmountText);
+        this.add(ticketAmountText,2,0);
+        this.getColumnConstraints().add(new ColumnConstraints(100,100,100));
 
         this.addTicket = new Button("ò");
         this.addTicket.getStyleClass().add("buttonRound");
@@ -87,7 +93,8 @@ public class Ticket extends HBox {
                 this.ticketAmount.set(this.ticketAmount.getValue() + 1);
             }
         });
-        this.getChildren().add(this.addTicket);
+        this.add(this.addTicket,3,0);
+        this.getColumnConstraints().add(new ColumnConstraints(70,70,70));
 
         this.ticketCost = new Text();
         this.ticketCost.setText(String.format("%.2f", this.parentController.getEvent().
@@ -96,8 +103,9 @@ public class Ticket extends HBox {
         this.ticketCost.setTextAlignment(TextAlignment.CENTER);
         this.ticketCost.setStrokeType(StrokeType.OUTSIDE);
         this.ticketCost.setStrokeWidth(0.0);
-        this.ticketCost.setWrappingWidth(97.0); //96.8671875??
-        HBox.setMargin(this.ticketCost, new Insets(0.0, 0.0, 0.0, 40.0));
-        this.getChildren().add(this.ticketCost);
+        this.ticketCost.setWrappingWidth(180); //96.8671875??
+        HBox.setMargin(this.ticketCost, new Insets(0.0, 0.0, 0.0, 0.0));
+        this.add(this.ticketCost,4,0);
+        this.getColumnConstraints().add(new ColumnConstraints(180,180,180));
     }
 }
